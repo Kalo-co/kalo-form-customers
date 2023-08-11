30ed7c20276dd3acca3f43533c2beab7cf040097
maarojasga <maarojasga@unal.edu.co>
2023-08-10 20:08:25 -0500

    Update endpoint developers

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
-from models import SessionLocal, User, engine
+from models import SessionLocal, User, Developer, engine
 import uvicorn
 
 app = FastAPI()
@@ -13,6 +13,18 @@ class UserCreate(BaseModel):
     company: str
     location: str
 
+class DeveloperCreate(BaseModel):
+    full_name: str
+    email: str
+    linkedin: str
+    age: int
+    country: str
+    formal_education: str
+    skills: str
+    work_experience: str
+    role: str
+    english_level: str
+    
 # Dependencia para obtener la sesión de DB
 def get_db():
     db = SessionLocal()
@@ -21,13 +33,27 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/customers/")
+@app.post("/customers/", status_code=status.HTTP_201_CREATED)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = User(**user.dict())
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+    try:
+        db_user = User(**user.dict())
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        return {"status": "success", "data": db_user}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@app.post("/developers/", status_code=status.HTTP_201_CREATED)
+def create_developer(dev: DeveloperCreate, db: Session = Depends(get_db)):
+    try:
+        db_dev = Developer(**dev.dict())
+        db.add(db_dev)
+        db.commit()
+        db.refresh(db_dev)
+        return {"status": "success", "data": db_dev}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 def init_db():
     from models import Base
--- models.py
+++ models.py
@@ -1,6 +1,9 @@
-from sqlalchemy import create_engine, Column, Integer, String, Sequence
+from sqlalchemy import Column, Integer, String, DateTime, create_engine, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from pydantic import BaseModel
+import uvicorn
+
 from config import DATABASE_URL
 
 Base = declarative_base()
@@ -12,6 +15,24 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     full_name = Column(String)
+    phone = Column(String)
     email = Column(String, unique=True)
     company = Column(String)
     location = Column(String)
+
+class Developer(Base):
+    __tablename__ = "developers"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    phone = Column(String)
+    full_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    linkedin = Column(String, index=True)
+    age = Column(Integer)
+    country = Column(String)
+    formal_education = Column(String)
+    skills = Column(String)
+    work_experience = Column(String)
+    role = Column(String)
+    english_level = Column(String)
