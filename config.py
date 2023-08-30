6a8c27bb7dd62a4e0f3d8d522f794c2462034aec
Mariale <66492316+maarojasga@users.noreply.github.com>
2023-08-29 19:45:14 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-DATABASE_URL = "postgresql://ptibrhuplursoj:2878ee064174c9d5e976b85bcd3cf6b76c7555e5a04d2d54f584879be59cf00b@ec2-3-217-146-37.compute-1.amazonaws.com:5432/dfrvu9ra6riu4j"
+DATABASE_URL = "postgres://txuljtqsqmqwhg:7cc7a5c7d28a14b84130aaa03cd43e22f8b2fb514177535c5088fd6926d8017d@ec2-18-211-215-8.compute-1.amazonaws.com:5432/d27tkv0k6vs6hb"
